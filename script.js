3ac72580d154f4191d224ea310da9972a13c3a8d
Sathiyamoorthi <145267450+sathiyamoorthipsnapsna@users.noreply.github.com>
2023-10-12 00:24:44 +0530

    Build a login page

--- script.js
+++ script.js
@@ -1,46 +1,45 @@
-function validateForm() {
-    const username = document.getElementById("username");
-    const email = document.getElementById("email");
-    const password = document.getElementById("password");
-    const confirmPassword = document.getElementById("confirm-password");
+function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-    const usernameError = document.getElementById("username-error");
-    const emailError = document.getElementById("email-error");
-    const passwordError = document.getElementById("password-error");
-    const confirmPasswordError = document.getElementById("confirm-password-error");
-
-    // Reset error messages
-    usernameError.textContent = "";
-    emailError.textContent = "";
-    passwordError.textContent = "";
-    confirmPasswordError.textContent = "";
-
-    let valid = true;
-
-    // Validate username (simple example)
-    if (username.value.length < 3) {
-        usernameError.textContent = "Username must be at least 3 characters.";
-        valid = false;
+    // Basic username and password validation
+    if (username === "" || password === "") {
+        alert("Username and password are required.");
+        return false;
     }
 
-    // Validate email (simple example)
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailPattern.test(email.value)) {
-        emailError.textContent = "Invalid email address.";
-        valid = false;
-    }
+    // Simulate password hashing (replace with actual hashing)
+    const hashedPassword = hashPassword(password);
 
-    // Validate password
-    if (password.value.length < 6) {
-        passwordError.textContent = "Password must be at least 6 characters.";
-        valid = false;
+    // Check the hashed password (replace with your authentication logic)
+    if (checkPassword(username, hashedPassword)) {
+        alert("Login successful!");
+        return true;
+    } else {
+        alert("Invalid username or password.");
+        return false;
     }
+}
 
-    // Confirm password
-    if (password.value !== confirmPassword.value) {
-        confirmPasswordError.textContent = "Passwords do not match.";
-        valid = false;
-    }
+function hashPassword(password) {
+    // In a real application, you would use a secure hashing algorithm
+    // This is a simplified example using SHA-256 (not recommended for production)
+    const hash = sha256(password);
+    return hash;
+}
 
-    return valid;
+function checkPassword(username, hashedPassword) {
+    // In a real application, you would check the stored hashed password for the given username
+    // This is a simplified example, you should replace it with your authentication logic
+    return username === "demo" && hashedPassword === hashPassword("demo123");
+}
+
+// Simple SHA-256 hash function (not suitable for production)
+function sha256(input) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(input);
+    return crypto.subtle.digest("SHA-256", data).then(hashBuffer => {
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
+        return hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+    });
 }
